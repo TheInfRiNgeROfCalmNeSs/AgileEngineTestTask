69f825899cd54ad0226f5b45c064a52899fd8574
TheInfRiNgeROfCalmNeSs <theinfringerofcalmness@gmail.com>
2018-08-06 16:38:59 +0300

    replacing selected word with synonym functionality

--- src/file-zone/FileZone.js
+++ src/file-zone/FileZone.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import './FileZone.css';
 
 class FileZone extends Component {
+    replaceSelectedWordWithSynonym(e) {
+        //console.log('replaceSelectedWordWithSynonym', this.props.selectedWord, e.target.dataset.synonym, this.props.synonymsArray.filter((synonym, key) => key===parseInt(e.target.dataset.synonym, 10)?synonym:null)[0].word)
+        let selectedWord = this.props.selectedWord;
+        let replacingSynonym = this.props.synonymsArray.filter((synonym, key) => key===parseInt(e.target.dataset.synonym, 10)?synonym:null)[0].word; 
+        if(selectedWord.children[0]!==undefined&&selectedWord.children.length===1) {
+            console.log('if 1')
+            if(selectedWord.children[0].children[0]!==undefined&&selectedWord.children[0].children.length===1) {
+                console.log('if 2')
+                if(selectedWord.children[0].children[0].children[0]!==undefined&&selectedWord.children[0].children[0].children.length===1) {
+                    console.log('if 3')
+                    selectedWord.children[0].children[0].children[0].innerText = replacingSynonym;
+                } else {
+                    console.log('else 3')
+                    selectedWord.children[0].children[0].innerText = replacingSynonym;
+                }
+            } else {
+                console.log('else 2')
+                selectedWord.children[0].innerText = replacingSynonym;
+            }
+        } else {
+            console.log('else 1', selectedWord.children[0], selectedWord.children.length)
+            selectedWord.innerText = replacingSynonym;
+        }
+    }
     render() {
     	//console.log('textInDivs', this.props.textToEditor)
         return (
@@ -14,7 +38,7 @@ class FileZone extends Component {
                                 return (
                                     <div key={key} className="synonym">
                                         word: {synonym.word}, score: {synonym.score}
-                                        <button className="format-action" type="button">Replace</button>
+                                        <button className="format-action" type="button" data-synonym={key} onClick={this.replaceSelectedWordWithSynonym.bind(this)}>Replace</button>
                                     </div>
                                 )
                             })
